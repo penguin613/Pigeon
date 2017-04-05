be15005582dba5f57cdf483c93c44958a494f521
서강민 <tjrkdals613@gmail.com>
2017-04-05 15:22:46 +0900

    2017-04-05 비둘기 첫 커밋

---
+++ app/src/main/java/practice/seokangmin/com/pigeon/MainActivity.java
@@ -0,0 +1,83 @@
+package practice.seokangmin.com.pigeon;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+import static practice.seokangmin.com.pigeon.R.id.editText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText gugu;
+
+    InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gugu = (EditText)findViewById(editText);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-8724322165606086~2327723056");
+
+        AdView mAdView = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-8724322165606086/3890024654");
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+            }
+        });
+
+        requestNewInterstitial();
+    }
+
+    public void onButtonGuClicked (View v){
+
+        String phoneNumber = "+82 1031885953";
+        String smsBody = gugu.getText().toString();
+        gugu.setText("");
+
+        try
+        {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNumber, null, smsBody, null, null);
+            //Toast.makeText(getApplicationContext(),"구~",Toast.LENGTH_LONG).show();
+
+        }
+        catch(Exception e)
+        {
+            Toast.makeText(getApplicationContext(), e.getStackTrace().toString(), Toast.LENGTH_LONG).show();
+        }
+
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Toast.makeText(getApplicationContext(), "광고 로딩 실패", Toast.LENGTH_SHORT);
+        }
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+}
